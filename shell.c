7f26b2b43cc25ceaf78dfed93e83cb1ad75c37f5
Jennifer <jyu6@stuy.edu>
2016-12-02 20:34:24 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -4,136 +4,159 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
 
-char **tokensplit(char *input) {
-  char **arr = (char **)(malloc(100));
-  char *temp = input;
-
-  int i = 0;
-  while (temp) {
-    arr[i] = strsep(&temp, " ");
-    i++;
-  }
-  arr[i] = NULL;
-
-  return arr;
-}
-
+/**
+ * arguments:
+ *  - user input (terminal commands)
+ *  - symbol used to separate (semi-colon)
+ * return value:
+ *  - arr of separated terminal commands
+ * function:
+ *  - splits up commands separated by a symbol
+**/
 char **splitnsep(char *input, char *sym) {
-  char *temp = input;
 
+  //INIT
+  char *temp = input;
   int i = 0;
   char **arr = (char **)(malloc(100));
 
-//SEPARATE
+  //SEPARATE
   while (temp) {
     arr[i] = strsep(&temp, sym);
     i++;
   }
-  arr[i] = NULL;
+  arr[i] = NULL; //NULL TERMINATE
 
+  //RETURN
   return arr;
+
 }
 
-void forkpls(char *arr[]) {
-  int f = fork();
-  if ( f == 0 ) {
-    execvp(arr[0], arr);
-  }
-  else {
-    wait(&f);
-  }
-}
+
 
 /**
-void greaterThan(char *command){
-  char *file = (char *)malloc(256);
-  file = command [loc+1];
-  int fd = open(line, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-  int ft = dup( STDOUT_FILENO );
-  dup2(fd, STDOUT_FILENO);
-  forkpls(file);
-  dup2(ft, STDOUT_FILENO);
-  close(fd);
-  free(file);
-}
+ * arguments:
+ *  - arr of separated terminal commands
+ * return value:
+ *  - N/A
+ * function:
+ *  - forks and executes the command
 **/
+void forkpls(char *arr[]) {
 
+  //FORK
+  int f = fork();
+
+  //EXECUTE
+  if ( f == 0 ) { //CHILD
+    execvp(arr[0], arr);
+  }
+  else { //PARENT
+    wait(&f);
+  }
+
+}
+
+
+
+/**
+ * arguments:
+ *  - a string with terminal commands
+ * return value:
+ *  - string without unnecessary spaces
+ * function:
+ *  - gets rid of spaces so it can be executed
+**/
 char *fireblanks(char *input) {
-  char * temp = (char*)malloc(100);
-  int i;
-  for (i = 0; input[i] != '\0'; i++) {
-    if ( !(input[i] == ' ' && (strlen(temp)==0 || input[i+1] == '\0' || input[i+1] == ' ')) ) {
+
+  //INIT
+  char *temp = (char*)(malloc(100));
+  int i = 0;
+
+  //
+  while(input[i]) {
+    if ( !(input[i]==' ' && (strlen(temp)==0 || input[i+1] == ' ' || !(input[i+1]))) ) {
       sprintf(temp, "%s%c", temp, input[i]);
     }
+    i++;
   }
+
+  //RETURN
   return temp;
 }
 
+
+
 int main() {
 
-while (1) {
-  printf("What would you like to do? ");
-  char input[60];
-  fgets(input, sizeof(input), stdin);
-  input[strlen(input)-1] = 0;
+  //INFINITE LOOP UNTIL YOU EXIT
+  while (1) {
 
-  char **pieces;
-  pieces = splitnsep(input, ";");
-/**
-  // IDK HOW TO IMPLEMENT THIS PROB NOT GONNA
-  int i = 0;
-  while (pieces[i]) {
-    pieces = (char**)splitnsep(pieces[i], " ");
-    i++;
-  }
+    //TAKES INPUT
+    printf("What would you like to do? ");
+    char input[60];
+    fgets(input, sizeof(input), stdin);
+    input[strlen(input)-1] = 0;
 
-**/
+    //SEPARATES BY SEMICOLON
+    char **pieces;
+    pieces = splitnsep(input, ";");
 
-//PRINT PIECES
 
+  //PRINT PIECES
   int i;
     for (i = 0; pieces[i] != NULL; i++) {
-      char * command_nonsplit = (char *)(malloc(100));
-      char ** command = (char **)(malloc(100));
+      char * c1 = (char*)(malloc(100));
+      char ** c2 = (char**)(malloc(100));
 
-      command_nonsplit = fireblanks( pieces[i] );
-      command = splitnsep(command_nonsplit, " ");
-      /**
-  while (pieces[i]) {
-    printf("SC: %s\n", pieces[i]);
-    i++;
-  }
-  **/
+      c1 = fireblanks( pieces[i] );
+      c2 = splitnsep(c1, " ");
 
- // forkpls(pieces);
-
-      if( !strcmp(command[0], "exit") ) {
-	        exit(0);
-
+      char *cdstr;
+      //EXIT
+      if( !strcmp(c2[0], "exit") ) {
+	      exit(0);
       }
 
-      else if ( !strcmp(command[0], "cd") ) {
-          chdir("..");
-}
-          else if ( !strcmp(command[0], ">") ) {
-         // greaterThan(command[0]);
-          }
-
-   else {
-	forkpls(command);
+      //CD
+      else if ( !strcmp(c2[0], "cd") ) {
+        cdstr = c2[1];
+        /**
+        if (cdstr = " ") {
+          cdstr = ".";
+        }
+        **/
+        chdir(cdstr);
       }
 
+      //REDIRECTOUT
+      else if ( !strcmp(c2[0], ">") ) {
+        // redirectout(command[0]);
+      }
+      //REDIRECTIN
+      else if ( !strcmp(c2[0], "<") ) {
+        // redirectin(command[0]);
+      }
+      else if ( !strcmp(c2[0], "|") ) {
+        // pipes(command[0]);
+      }
+      else {
+
+        //EXECUTE NORMALLY
+  	    forkpls(c2);
+      }
     }
   }
 
+  //END MAIN FUNCTION
   return 0;
+
 }
 
 /**
- * GARBAGE DUMP
+ * GARBAGE DUMPPP
 **/
   /**
   int j = 0;
@@ -171,3 +194,45 @@ while (1) {
       printf("Error %d: %s\n", errno, strerror(errno));
     }
 **/
+/**
+char **tokensplit(char *input) {
+  char **arr = (char **)(malloc(100));
+  char *temp = input;
+
+  int i = 0;
+  while (temp) {
+    arr[i] = strsep(&temp, " ");
+    i++;
+  }
+  arr[i] = NULL;
+
+  return arr;
+}
+**/
+/**
+void greaterThan(char *command){
+  char *file = (char *)malloc(256);
+  file = command [loc+1];
+  int fd = open(line, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+  int ft = dup( STDOUT_FILENO );
+  dup2(fd, STDOUT_FILENO);
+  forkpls(file);
+  dup2(ft, STDOUT_FILENO);
+  close(fd);
+  free(file);
+}
+**/
+/**
+  // IDK HOW TO IMPLEMENT THIS PROB NOT GONNA
+  int i = 0;
+  while (pieces[i]) {
+    pieces = (char**)splitnsep(pieces[i], " ");
+    i++;
+  }
+**/
+      /**
+  while (pieces[i]) {
+    printf("SC: %s\n", pieces[i]);
+    i++;
+  }
+  **/
