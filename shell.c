56741224cf257edf801c839782807d3c5a701ca0
parkjmjohn <parkjmjohn@gmail.com>
2016-12-04 01:41:22 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,14 +5,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include "readline/readline.h"
+#include "readline/history.h"
 
 /**
- * arguments:
+ * Arguments:
  *  - user input (terminal commands)
  *  - symbol used to separate (semi-colon)
- * return value:
+ * Return value:
  *  - arr of separated terminal commands
- * function:
+ * Function:
  *  - splits up commands separated by a symbol
 **/
 char **splitnsep(char *input, char *sym) {
@@ -21,7 +24,6 @@ char **splitnsep(char *input, char *sym) {
   char *temp = input;
   int i = 0;
   char **arr = (char **)(malloc(100));
-
   //SEPARATE
   while (temp) {
     arr[i] = strsep(&temp, sym);
@@ -34,14 +36,12 @@ char **splitnsep(char *input, char *sym) {
 
 }
 
-
-
 /**
- * arguments:
+ * Arguments:
  *  - arr of separated terminal commands
- * return value:
+ * Return value:
  *  - N/A
- * function:
+ * Function:
  *  - forks and executes the command
 **/
 void forkpls(char *arr[]) {
@@ -59,14 +59,12 @@ void forkpls(char *arr[]) {
 
 }
 
-
-
 /**
- * arguments:
+ * Arguments:
  *  - a string with terminal commands
- * return value:
+ * Return value:
  *  - string without unnecessary spaces
- * function:
+ * Function:
  *  - gets rid of spaces so it can be executed
 **/
 char *fireblanks(char *input) {
@@ -87,15 +85,102 @@ char *fireblanks(char *input) {
   return temp;
 }
 
+/**
+ * Arguments:
+ *  - Command and the argument you are searching for
+ * Return value:
+ *  - location of the entered argument
+ * Function:
+ *  - Returns the index of the argument you are searching for, returns false otherwise
+**/
+int searcher(char *input[], char *search){
+  int i;
+  for(i=0; input[i]; i++){
+    if(!(strcmp(input[i], search))){
+      return i;
+    }
+  }
+  return 0;
+}
 
+
+/**
+ * Arguments:
+ *  - The command you input
+ * Return value:
+ *  - N/A (it will run the command)
+ * Function:
+ *  - Searches the type of sign entered. Based on thatit will split the input
+ *    by the sign. It will perform the approrpiate redirection before executing
+ *    the command
+**/
+// void redir(* input){
+//   int pos;
+//   if ((pos = searcher(input, ">"))){
+//     char *temp = input[pos+1];
+//     input[pos] = 0;
+//     int fd = open(temp, O_CREAT | O_RDWR, 0644);
+//     dup2 (fd, STDOUT_FILENO);
+//     forlpls (input);
+//   }
+//   else if((pos = searcher(input,">>"))){
+//     char *temp = input[pos+1];
+//     input[pos] = 0;
+//     int fd = open(temp, O_APPEND | O_CREAT | O_RDWR, 0644);
+//     dup2 (fd, STDOUT_FILENO);
+//     forkpls (input);
+//   }
+//   else if((pos = searcher(input,"2>"))){
+//     char *temp = input[ pos + 1];
+//     input[pos] = 0;
+//     int fd = open(temp, O_CREAT | O_RDWR, 0644);
+//     dup2 (fd, STDERR_FILENO);
+//     forkpls(input);
+//   }
+//   else if( (pos = searcher(input,"&>")) ) {
+//     char *temp = input[ pos + 1];
+//     input[pos] = 0;
+//     int fd =open(temp, O_CREAT | O_RDWR, 0644);
+//     dup2 (fd, STDOUT_FILENO);
+//     dup2 (fd, STDERR_FILENO);
+//     forkpls (input);
+//   }
+//   else if( (pos = searcher(input,"<")) ) {
+//     char *temp = input[ pos + 1];
+//     input[pos] = 0;
+//     int fd = open(temp, O_CREAT | O_RDWR, 0644);
+//     dup2(fd,STDIN_FILENO);
+//     forkpls(input);
+//   }
+//   else if((pos = searcher(input,"|"))) {
+//     char **temput = &input[pos+1];
+//     input[pos] = 0;
+//     int fds[2];
+//     pipe(fds);
+//   	int f2;
+//     f2 = fork();
+//     if(!f2){
+//       close( fds[0] );
+//   	  dup2( fds[1], STDOUT_FILENO );
+//   	  forkpls( input, f );
+//     }else{
+//   	  int timer
+//   	  wait(&timer);
+//   	  close(fds[1]);
+//   	  dup2(fds[0], STDIN_FILENO);
+//   	  forkpls(temput);
+//     }
+//   }
+// }
 
 int main() {
 
   //INFINITE LOOP UNTIL YOU EXIT
   while (1) {
-
     //TAKES INPUT
-    printf("What would you like to do? ");
+    char pwd[256];
+    getcwd(pwd, sizeof(pwd));
+    fprintf(stdout, "%s: ", pwd);
     char input[60];
     fgets(input, sizeof(input), stdin);
     input[strlen(input)-1] = 0;
@@ -132,27 +217,16 @@ int main() {
       }
 
       //REDIRECTOUT
-      else if ( !strcmp(c2[0], ">") ) {
-        // redirectout(command[0]);
-      }
-      //REDIRECTIN
-      else if ( !strcmp(c2[0], "<") ) {
-        // redirectin(command[0]);
-      }
-      else if ( !strcmp(c2[0], "|") ) {
-        // pipes(command[0]);
-      }
-      else {
-
+      else if (strchr(c2[0], '>') || strchr(c2[0], '<') || strchr(c2[0], '|')) {
+      //redir(c2[0]);
+      } else {
         //EXECUTE NORMALLY
   	    forkpls(c2);
       }
     }
   }
-
   //END MAIN FUNCTION
   return 0;
-
 }
 
 /**
